f526199ac283fd3a424b56a717664525c3015f12
Joe Hartley <hartleyljoseph@gmail.com>
2025-03-21 16:57:26 +0000

    ahhhhhhh

--- iotbike/iotbike.py
+++ iotbike/iotbike.py
@@ -8,11 +8,11 @@ def api_post(data, suffix, url="http://joehartley.pythonanywhere.com"):
 
     response = requests.post(url + suffix, json=data)
 
-    return response.json()
+    return response
 
 def api_get(suffix, url="http://joehartley.pythonanywhere.com"):
     response = requests.get(url + suffix)
-    return response.json()
+    return response
 
 
 def main(pi=True, source=0):
